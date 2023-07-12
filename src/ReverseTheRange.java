d96ac231f6cc6fef510900a3cc762f1ef82874f0
NAAGAVARSHINI <naagavarshinim@gmail.com>
2023-07-12 15:32:18 +0530

    first commit

---
+++ src/ReverseTheRange.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ReverseTheRange {
+
+    public static int[] Reverse(int array[], int b, int c){
+        int size = array.length;
+        int i= b;
+        int j=size-1;
+        while(i<j){
+            int temp = array[i];
+            array[i]= array[j];
+            array[j]= temp;
+            i++;
+            j--;
+
+        }
+        return array;
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int array[] = new int[n];
+        for(int i=0;i<n;i++){
+            array[i]=sc.nextInt();
+        }
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+
+        array= Reverse(array, b,c);
+        for(int i=0;i<n;i++){
+            System.out.print(array[i] + " ");
+        }
+
+    }
+}
